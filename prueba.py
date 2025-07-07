e4011e8844335df4dea42e51a3346d7659657cab
SantiagoSalomRSM <ssalom@rsm.es>
2025-07-07 13:48:39 +0200

    sd

--- prueba.py
+++ prueba.py
@@ -9,5 +9,8 @@ client = OpenAI(api_key=API_KEY)
 
 response = client.responses.create(
     model="gpt-4.1",
-    input="explain etinction of dinosaurs")
+    input="one doubt: i currently have a project that works the following way: i have a form that is filled by a CFO with tally. the response is then processed by an api deployed in vercel, whi" \
+    "ch generates a prompt and sends it to gemini or other ai agents. the responses are then stored in a supabase database. then a streamlit app retrieves the data from supabase and displays " \
+    "it in a user-friendly format. i want to know if this can be done with azure, mantaining tally because it allows to trigger webhooks and redirect on completion, but changing everything else.")
+
 print(response.output_text)
